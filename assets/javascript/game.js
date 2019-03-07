e13c563e58ac90867bd8355440f4d4c6603b251b
Jessie Sidhu <jsidhu22@gmail.com>
2019-03-06 17:17:36 -0800

    JS is pretty much finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,14 +30,28 @@ $(document).ready(function(){
   $("#questions").hide();
   $("#timer").hide();
   $("#reset").hide();
+  $("#start").show();
+
+
+  // Answer Key
+  const answerKey = {
+
+    question1: 2,
+    question2: 3,
+    question3: 1,
+    question4: 3,
+    question5: 2,
+    question6: 3,
+
+  };
   
-  // variables
+  // Variables
   var correctAnswers = 0;
   var wrongAnswers = 0;
   var timeStart = 60;
   var intervalId;
   
-  // reset function
+  // Reset function
   var intializeReset = function(){
     correctAnswers = 0;
     wrongAnswers = 0;
@@ -51,11 +65,19 @@ $(document).ready(function(){
     $("#reset").hide();
     $("#start").show();
 
+    $('#reset').click(function () {
+
+      $(".answers").each(function(i) 
+       {
+            this.checked = false;
+       });
+  
+    });
   }
+
 
   // Timer function
   function decrement() {
-
     timeStart--;
 
     if (timeStart > 9) {
@@ -63,7 +85,6 @@ $(document).ready(function(){
     } else if (timeStart < 10) {
       $("#timer").html("00:0" + timeStart);  
     }
-
     if (timeStart === 0) {
       clickSubmit();
     }
@@ -72,56 +93,55 @@ $(document).ready(function(){
   // START button click
   $("#start").on("click", function(){
     // console.log("You click the start button!");
-
     $("#start").hide();
     $("#questions").show();
     $("#timer").show();
+    $("#reset").hide();
 
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
-
     decrement(); 
   });
 
+
+  // Submit Function
   var clickSubmit = function(){
 
     $("#start").hide();
     $("#questions").hide();
     $("#timer").hide();
-    $("reset").show();
-
-    for(var i = 1; i <= 6; i++) {
-      var radios = document.getElementsByName('q'+i);
-      for(var j = 0; j < radios.length; j++) {
-        var radio = radios[j];
-        if(radio.value == "correct" && radio.checked) {
-          correctAnswers++;
-        } else {
-          wrongAnswers++;
-        }
+    $("#reset").show();
+    clearInterval(intervalid);
+    $(".answers").each(function(){
+      // console.log($(this).is(":checked"));
+      
+      if ($(this).is(":checked") === true) {
+            
+            var q = $(this).attr("name");
+            var ans = $(this).attr("data-ans");
+          if (ans == answerKey[q]) {
+            correctAnswers++;
+          } else {
+            wrongAnswers++
+          }
       }
+    });
 
-      $("#correct").html("You got " + correctAnswers + " right!");
-      $("#wrong").html("You got " + wrongAnswers + " wrong!");
-
-    };
-
+    $("#correct").html("You got " + correctAnswers + " right!");
+    $("#wrong").html("You got " + wrongAnswers + " wrong!");
+    
   }
 
+
   // SUBMIT button click
-  $("#submit").on("click", clickSubmit);
+    $("#submit").on("click", clickSubmit);
 
   // RESET button click
   $("#reset").on("click", function(){
-
+    // console.log("You clicked the Reset Button!");
     intializeReset();
-
   });
 
-
-
-  
-
-
+  intializeReset();
 
 });
---
+++ assets/javascript/info.js
@@ -0,0 +1,13 @@
+
+
+
+const answerKey = {
+
+    question1: 2,
+    question2: 3,
+    question3: 1,
+    question4: 3,
+    question5: 2,
+    question6: 3
+
+};
